04728da9516810e854d8716399a8994de2347710
rr-jino-jose <jjose@ebates.com>
2023-07-27 20:54:58 +0530

    JS action :rocket:

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ async function run() {
     await executeCommand(`curl -LO https://github.com/kubeshop/testkube/releases/download/v${testkubeVersion}/testkube_${testkubeVersion}_Linux_x86_64.tar.gz`);
     await executeCommand(`tar -xvf testkube_${testkubeVersion}_Linux_x86_64.tar.gz`);
     await executeCommand(`mv kubectl-testkube /usr/local/bin/`);
+    await executeCommand(`testkube version`);
 
     core.info('Testkube installation completed successfully!');
   } catch (error) {
